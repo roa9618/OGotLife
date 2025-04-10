7b7429302813b5477115726d2df9a7a9197f1d7c
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-04-10 17:59:20 +0900

    feat: Add basic Footer component with initial styling

---
+++ src/components/footer.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "../styles/footer.css";
+
+function Footer() {
+    return (
+        <footer className = "footer">
+            <div>
+                <p>© 2025 OGotLife. All rights reserved.</p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
