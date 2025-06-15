fbba49ceb09df9b4ce4d7a83e76830807cdc2d55
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-06-15 16:04:24 +0900

    [추가] 사용자 이름 옆 아이콘 추가

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,6 +3,7 @@ import '../styles/Header.css';
 import logo_image from '../assets/ogotlife_logo.png';
 import alarm_image from '../assets/alarm_image.png';
 import profile_image from '../assets/default_profile_image.png';
+import profile_side_icon from '../assets/header_profile_side_image.png';
 
 function Header() {
     return (
@@ -25,6 +26,7 @@ function Header() {
                 <img src = {alarm_image} alt = "alarm" className = "alarm_image"/>
                 <img src = {profile_image} alt = "profile" className = "profile_image"/>
                 <a href = "#">홍길동</a>
+                <img src = {profile_side_icon} alt = "profile_side_icon" className = "profile_side_icon"/>
             </div>
         </header>
     );
