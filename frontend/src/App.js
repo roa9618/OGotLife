3d95f256091760b35e674868b0f1606e4cab4e17
Woohyeon Roh <nwh0326@yu.ac.kr>
2025-06-16 10:06:55 +0900

    [추가] 페이지 라우팅 설정 구문 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,26 @@
 import React from 'react';
 import './styles/App.css';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import Dashboard from './pages/Dashboard';
+import Routine from './pages/Routine';
+import Todo from './pages/Todo';
+import Record from './pages/Record';
+import Calendar from './pages/Calendar';
+import FocusMode from './pages/FocusMode';
+import Community from './pages/Community';
 
 function App() {
   return (
     <Router>
       <Routes>
+        <Route path="/" element={<Navigate to="/dashboard"/>}/>
         <Route path="/dashboard" element={<Dashboard/>}/>
+        <Route path="/routine" element={<Routine/>}/>
+        <Route path="/todo" element={<Todo/>}/>
+        <Route path="/record" element={<Record/>}/>
+        <Route path="/calendar" element={<Calendar/>}/>
+        <Route path="/focus" element={<FocusMode/>}/>
+        <Route path="/community" element={<Community/>}/>
       </Routes>
     </Router>
   );
